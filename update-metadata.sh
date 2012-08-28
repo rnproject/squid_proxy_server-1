0b78aefa47087d108ec9a359136ca0c86a28ba32
Chris Fordham <chris@xhost.com.au>
2012-08-28 15:57:46 +1000

    Comment out metadata re-gen for squid (metadata.json now exists upstream).

--- update-metadata.sh
+++ update-metadata.sh
@@ -5,4 +5,4 @@ knife cookbook metadata collectd -o cookbooks/
 knife cookbook metadata collectd_plugins -o cookbooks/
 knife cookbook metadata cron -o cookbooks/
 knife cookbook metadata ntp -o cookbooks/
-knife cookbook metadata squid -o cookbooks/
+#knife cookbook metadata squid -o cookbooks/
