75bf1e347e01be4f35399775a7f358e9a218f35b
Chris Fordham <chris.fordham@rightscale.com>
2012-08-28 15:18:42 +1000

    Add update-metadata.sh script.

---
+++ update-metadata.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+# generate metadata.json for upstream opscode cookbooks that have no metadata.json
+knife cookbook metadata collectd -o cookbooks/
+knife cookbook metadata collectd_plugins -o cookbooks/
+knife cookbook metadata cron -o cookbooks/
+knife cookbook metadata ntp -o cookbooks/
+knife cookbook metadata squid -o cookbooks/
