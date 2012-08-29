4c2bbad50a6adea9ec7882fa060b9c7ed5943dae
Chris Fordham <chris.fordham@rightscale.com>
2012-08-29 18:27:43 +1000

    Add remove-dot-git-from-cookbooks.sh helper script.

---
+++ remove-dot-git-from-cookbooks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+rm -Rf ./cookbooks/collectd/.git
+rm -Rf ./cookbooks/collectd_plugins/.git
+rm -Rf ./cookbooks/cron/.git
+rm -Rf ./cookbooks/ntp/.git
+rm -Rf ./cookbooks/squid/.git
+rm -Rf ./cookbooks/sudo/.git
